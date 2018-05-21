f2586fb0484167c99fa2da504382e597742835f3
Courtney Brothers <courtneywbrothers@gmail.com>
2018-05-21 11:47:03 -0500

    stock dictionary exercise

---
+++ stocks.py
@@ -0,0 +1,16 @@
+
+purchases = [ ( 'GE', 100, '10-sep-2001', 48 ),
+ ( 'CAT', 100, '1-apr-1999', 24 ),
+ ( 'GE', 200, '1-jul-1998', 56 ),
+ ( 'AB', 100, '1-jul-2002', 12 ),
+ ( 'CB', 500, '2-jan-1995', 1 ),
+ ( 'MB', 2000, '27-jul-2001', 1000 )  ]
+
+stockDict = { 'GM': 'General Motors',
+ 'CAT':'Caterpillar', 'GE':"General Electric", "AB":"Another Business","CB":"Courtney Brothers", "MB":"My Business"}
+
+for purchase in purchases:
+  for key, name in stockDict.items():
+    if purchase[0] == key:
+      price = purchase[1]*purchase[3]
+      print(name + " {0} date:{1}".format(price,purchase[3]))
